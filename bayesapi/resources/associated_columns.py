baed05055faf687bd41984712e2636cf30da3773
Zane Shelby <zane@users.noreply.github.com>
2018-07-11 20:40:55 -0400

    Return scores in addition to names, normalize columns internally (#91)

--- bayesapi/resources/associated_columns.py
+++ bayesapi/resources/associated_columns.py
@@ -5,11 +5,22 @@ from bayesapi.resources import BaseResource
 from bayesapi.validation import validate
 
 class AssociatedColumnsResource(BaseResource):
-
     def on_post(self, req, resp):
+        table_name = self.cfg.table_name
 
         req_vars = validate(self.api_def, 'post', req)
         target_column = req_vars['column']
+
+        def normalize(column):
+            return column.lower()
+
+        with self.bdb.savepoint():
+            def column_names():
+                query = self.queries.get_full_table(table_name=table_name)
+                cursor = self.execute(query)
+                return [tuple[0] for tuple in cursor.description]
+
+            case_mapping = {normalize(column): column for column in column_names()}
 
         with self.bdb.savepoint():
             create_depprob_table_query = self.queries.create_dependence_probability_table(
@@ -18,15 +29,15 @@ class AssociatedColumnsResource(BaseResource):
 
             self.execute(create_depprob_table_query)
 
-            query = self.queries.select_dependence_probabilities(column_name=target_column.lower())
+            query = self.queries.select_dependence_probabilities(column_name=normalize(target_column))
             result = self.execute(query)
 
-            order = [row[0] for row in result.fetchall()]
+            maps = [{'column': case_mapping[row[0]], 'score': row[1]}
+                for row
+                in result.fetchall()
+                if case_mapping[row[0]] != target_column]
 
-        # target column always comes first
-        if target_column in order:
-            order.remove(target_column)
-        order.insert(0, target_column)
+        maps.insert(0, {'column': target_column, 'score': 1.0})
 
-        resp.media = order
+        resp.media = maps
         resp.status = 200
